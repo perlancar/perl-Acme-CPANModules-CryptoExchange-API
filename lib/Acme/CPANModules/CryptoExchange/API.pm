9061221c4a15e1470b27652540248a9c3d8d3d18
perlancar (on pc-office) <perlancar@gmail.com>
2018-01-09 17:57:11 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/CryptoExchange/API.pm
@@ -0,0 +1,43 @@
+package Acme::CPANModules::CryptoExchange::API;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Modules that interface to cryptocurrency exchanges",
+    entries => [
+        {
+            module => 'WebService::Cryptopia',
+        },
+        {
+            module => 'WebService::Binance',
+        },
+        {
+            module => 'Finance::BTCIndo',
+        },
+        {
+            module => 'Finance::BitFlip',
+        },
+        {
+            module => 'Finance::Bank::Kraken',
+        },
+        {
+            module => 'Poloniex::API',
+        },
+        {
+            module => 'WWW::API::Bitfinex',
+        },
+        {
+            module => 'Finance::BitStamp::API',
+        },
+        {
+            module => 'Finance::GDAX::API',
+        },
+        {
+            module => 'Finance::LocalBitcoins::API',
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
